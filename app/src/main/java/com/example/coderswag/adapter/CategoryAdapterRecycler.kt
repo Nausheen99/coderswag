4bf03e88a1e491f9be52e655f655a536d8d3a349
nausheen99 <naushabbas@gmail.com>
2022-04-19 11:00:30 +0500

    recycler view

---
+++ app/src/main/java/com/example/coderswag/adapter/CategoryAdapterRecycler.kt
@@ -0,0 +1,41 @@
+package com.example.coderswag.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coderswag.R
+import com.example.coderswag.databinding.ActivityMainBinding.bind
+import com.example.coderswag.model.Category
+import com.example.coderswag.service.dataServices.categories
+
+class CategoryAdapterRecycler (val context: Context,val categories: List<Category>) :  RecyclerView.Adapter<CategoryAdapterRecycler.Holder>() {
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val categoryImage = itemView.findViewById<ImageView>(R.id.catimage)
+        val categoryName = itemView.findViewById<TextView>(R.id.catname)
+
+        fun bindCategory(category:Category, context: Context ){
+            val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+            categoryImage?.setImageResource(resourceId)
+            categoryName?.text = category.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.category_list_item, parent, false)
+        return Holder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bindCategory(categories[position], context)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+}
